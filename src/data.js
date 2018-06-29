f9107cd02290b00587e7435c84313151184c34f2
Mhelodie <mhelodie_8_07@hotmail.com>
2018-06-29 17:18:05 -0500

    Readme actualizado

---
+++ src/data.js
@@ -0,0 +1,9 @@
+window.computeUsersStats = (users, progress, courses) => {
+};
+
+window.sortUsers = (users, orderBy, orderDirection) => {
+    var ordenar = sort
+    
+};
+window.filterUsers = (users, filterBy) => {};
+window.processCohortData = ({ cohortData, orderBy, orderDirection, filterBy }) => {};
